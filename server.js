fdf159884f5eb159bd1c09d87065aadd9e2db09f
bill finger <15381399+bilfinger@users.noreply.github.com>
2021-03-17 11:50:58 +0100

    test hub

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 require       ( 'http'                                          )
-.createServer ( (request,response)  => response.end('hallo'   ) )
+.createServer ( (request,response)  => response.end('hallo1'  ) )
 .listen       ( 8080, 'localhost', () => console.log('running') )
 ;
